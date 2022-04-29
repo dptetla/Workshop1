d094783e0ded2c99560452dfa238f5d0db1b1f74
daniel <dptetla@gmail.com>
2022-04-29 17:11:48 +0200

    Branch first commit

--- Java/src/pl/coderslab/TaskManager.java
+++ Java/src/pl/coderslab/TaskManager.java
@@ -2,5 +2,28 @@ package pl.coderslab;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class TaskManager {
+    public static void options() {
+        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        String[] tab = {"add", "remove", "list", "exit"};
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        options();
+//        File file = new File("")
+//        Scanner scanner = new Scanner("tasks.csv"){
+//
+//        }
+    }
+
+
+
 }
+
+
